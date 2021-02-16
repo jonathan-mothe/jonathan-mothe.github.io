b8bb52225a6a15f7a15af88d539f1ac56d7153f1
jonathan-mothe <jonathan.mothe@gmail.com>
2021-02-16 15:04:29 -0300

    Update particles.js

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
 {
   "particles": {
     "number": {
-      "value": 80,
+      "value": 150,
       "density": {
         "enable": true,
         "value_area": 800
@@ -51,7 +51,7 @@ particlesJS('particles-js',
       }
     },
     "size": {
-      "value": 3,
+      "value": 5,
       "random": true,
       "anim": {
         "enable": false,
